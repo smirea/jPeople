24f9c6b263818904a324b4d9178a0e25cebd3492
smirea <steven.mirea@gmail.com>
2012-09-03 16:36:00 +0200

    added  and updated anchors

--- tips.php
+++ tips.php
@@ -19,13 +19,16 @@
       array( 'germany eecs', '<b>space( )</b> - means <b>AND</b> (everyone who is german <b>and</b> studies eecs' ),
       array( 'lname:john', '<b>semicolon(:)</b> - designates a strict query (i.e. will only search for people whose last name is "john")'),
       array( 'country:japan,finland', '<b>comma(,)</b> in a <i>Strict query</i> - means <b>OR</b> (everyone from Germany <b>or</b> Finland)' ),
-      array( '~alex romania', '<b>tilda(~)</b> <u>at the beginning of a word</u> - means <b>NOT</b> (all romanians who are not alex' ),
+      array( '~alex romania', '<b>tilda(~)</b> <u>at the beginning of a word</u> - means <b>NOT</b> (all romanians who are not alex)' ),
+      array( 'title:%president', '<b>percent sign(%)</b> <u>at the beginning of a word</u> - means <b>CONTAINS</b> (works only in <a href="#s-2">strict queries</a> and it means to look for attributes that contain the string raher than those which just start with the string). See the difference: <code>title:president</code>' ),
       //array( 'course:"advanced computer science"', '<b>double quotes(")</b> - take everything in between together (everyone who is taking that course this semester). Note, without the quotes it would have been: <i>everyone who takes the course advanced and is computer and science</i> :) )' )
     ),
+    /*
     'Common tips' =>
     array(
       array( 'major:cs major:~eecs major:~ics', 'some majors\' acronym is contained in others (e.g. CS and EECS,ICS) . Exclude the ones that you don\'t want')
     ),
+    */
     'List of all fields' =>
     array(
       array('fname:stefan', 'first name'),
@@ -47,7 +50,8 @@
       array('account:smirea', 'any account name'),
       array('office:"Research I, 100a"', 'office number'),
       array('deptinfo:physics', 'department information'),
-      array('attributes:president', 'any type of status (e.g. "Research Associate", technician, etc)')
+      array('title:president', 'any type of status (e.g. "Research Associate", technician, etc)'),
+      array('attributes:%president', 'different roles (e.g. President/Vice President)')
     )
   );
 
@@ -56,17 +60,18 @@ echo <<<HTML
 <div class="title">Search Functionality Tips &amp; Tricks</div>
 <div class="section" title="General Info">
 The search works by comparing each word you insert with a set of attributes specified bellow. For example if you insert : <code>germany</code> it will find all Germans on campus, but if you isert only <code>ger</code> it will find all Germans plus all people named Germaine for example or any other person which has that token in its attributes. <br />
-No algorithm can guess exactly what you really want, therefore if you really want to be sure of what you are searching either specify long exact words or use <a href="#">Strict queries</a> (which only search in the field you select)
+No algorithm can guess exactly what you really want, therefore if you really want to be sure of what you are searching either specify long exact words or use <a href="#s-2">Strict queries</a> (which only search in the field you select)
 </div>
 
 HTML;
 
-$c = 0;
+$sections = 0;
 foreach( $tips as $k => $v ){
   $c = 0;
+  ++$sections;
   echo <<<HTML
     <table class="section codeTable" cellspacing="0" cellpadding="0" title="$k">
-      <tr><td colspan="2" class="header">$k</td></tr>
+      <tr><td colspan="2" class="header" id="s-$sections">$k</td></tr>
 HTML;
   foreach( $v as $val ){
     $class = ++$c % 2 == 0 ? "even" : "odd";
