085916ef22b55982d2389cfc9e84cbeb96b45604
smirea <steven.mirea@gmail.com>
2012-09-04 13:27:34 +0000

    implemented tracking functions

---
+++ utils.php
@@ -0,0 +1,22 @@
+<?php
+
+  /** REQUIRES: config.php **/
+  require_once 'config.php';
+
+  function track ($column, $value, $failed = false, $error = '') {
+    $query = "INSERT INTO ".TABLE_TRACKING."(timestamp, ip, failed, error, query) ".
+                     "VALUES ('".time()."', '".get_ip_address()."', '$failed', '$error', '$value')";
+    return mysql_query($query);
+  }
+
+  function get_ip_address () {
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+      $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+      $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+      $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+  }
+?>
