56bfc697b7e18adbc6487d232ad5ffd6ea651eb2
smirea <steven.mirea@gmail.com>
2012-09-06 18:34:11 +0200

    better formatting

--- ajax.php
+++ ajax.php
@@ -15,24 +15,30 @@
     $str 		= $_GET['str'];
 
     if( strlen( $str ) < MIN_LIMIT ){
-      jsonOutput( array('error' => 'Query too short. Must have at least '.MIN_LIMIT.' chars') );
+      jsonOutput( array('error' => 'Query too short. Must have at least '.
+                                      MIN_LIMIT.' chars'
+      ));
     }
 
     switch($_GET['action']){
       case 'autoComplete':
         $columns  = "id,eid,fname,lname,country,major,college";
         if( $clause = $Search->getQuery($str) ){
-          $res      = mysql_query( "SELECT $columns FROM ".TABLE_SEARCH." WHERE $clause" );
+          $res      = mysql_query( "SELECT $columns FROM ".TABLE_SEARCH.
+                                    " WHERE $clause"
+          );
           sqlToJsonOutput( $res );
         } else {
           jsonOutput( array( 'error' => 'Invalid query' ) );
         }
       break;
       case 'fullAutoComplete':
-        $columns  = 'id,eid,employeetype,attributes,account,attributes,fname,lname,birthday,country,college,majorlong,'.
-                    'majorinfo,major,status,year,room,phone,email,description,title,office,deptinfo,block,floor';
+        $columns  = 'id,eid,employeetype,attributes,account,attributes,'.
+                      'fname,lname,birthday,country,college,majorlong,'.
+                      'majorinfo,major,status,year,room,phone,email,'.
+                      'description,title,office,deptinfo,block,floor';
         if( $clause = $Search->getQuery($str) ){
-          $query    = "SELECT $columns FROM ".TABLE_SEARCH." WHERE $clause"; 
+          $query    = "SELECT $columns FROM ".TABLE_SEARCH." WHERE $clause";
           $res      = mysql_query($query);
           if ($res) {
             $records  = sqlToArray($res);
@@ -58,14 +64,20 @@
         }
       break;
       case 'getFace':
-        $columns  = 'id,eid,employeetype,attributes,account,attributes,fname,lname,birthday,country,college,majorlong,'.
-                    'majorinfo,major,status,year,room,phone,email,description,title,office,deptinfo,block,floor';
-        $res      = mysql_query( "SELECT $columns FROM ".TABLE_SEARCH." WHERE eid='$str'" );
+        $columns  = 'id,eid,employeetype,attributes,account,attributes'.
+                      ',fname,lname,birthday,country,college,majorlong,'.
+                      'majorinfo,major,status,year,room,phone,email,'.
+                      'description,title,office,deptinfo,block,floor';
+        $res      = mysql_query( "SELECT $columns FROM ".TABLE_SEARCH.
+                                  " WHERE eid='$str'"
+        );
         sqlToJsonOutput( $res );
       break;
       case 'getAll':
         $columns  = "*";
-        $res      = mysql_query( "SELECT $columns FROM ".TABLE_SEARCH." WHERE ".$Search->getQuery($str) );
+        $res      = mysql_query( "SELECT $columns FROM ".TABLE_SEARCH."
+                                   WHERE ".$Search->getQuery($str)
+        );
         sqlToJsonOutput( $res );
       break;
       case 'vcf':
@@ -74,7 +86,7 @@
           if( !headers_sent() ){
             header('Content-Description: File Transfer');
             header('Content-Type: text/x-vcard; charset=utf-8');
-            header('Content-Disposition: attachment; filename=jPeople_contacts.vcf');
+            header('Content-Disposition: attachment; filename=jPeople.vcf');
             header('Expires: 0');
             header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
             header('Pragma: public');
