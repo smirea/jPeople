80c01596f41dc6bb129577697247dae704305623
smirea <steven.mirea@gmail.com>
2012-11-15 12:59:42 +0100

    added prototype admin page for statistics

---
+++ admin/ajax.php
@@ -0,0 +1,55 @@
+<?php
+
+  require_once "../config.php";
+
+  if (!isset($_GET['action'])) {
+    jsonOutput(array('error' => 'No action specified'));
+  }
+
+  switch ($_GET['action']) {
+    case 'browsers':
+      $query = mysql_query("SELECT DISTINCT browser as category, count(*) AS value
+                            FROM Tracking GROUP BY category
+                            ORDER BY value DESC"
+      );
+      sqlToJsonOutput($query);
+      break;
+    case 'OS':
+      $query = mysql_query("SELECT DISTINCT os as category, count(*) AS value
+                            FROM Tracking GROUP BY category
+                            ORDER BY value DESC"
+      );
+      sqlToJsonOutput($query);
+      break;
+    case 'days-and-hours':
+      $query = mysql_query("SELECT time FROM Tracking");
+      $data = sqlToArray($query);
+      $days = array();
+      $hours = array();
+      foreach ($data as $value) {
+        $tmp = explode(' ', $value['time']);
+
+        if (!isset($days[$tmp[0]])) {
+          $days[$tmp[0]] = 0;
+        }
+        ++$days[$tmp[0]];
+
+        $time = explode(':', $tmp[1]);
+        $hour = intval($time[0]);
+        if (!isset($hours[$hour])) {
+          $hours[$hour] = 0;
+        }
+        ++$hours[$hour];
+      }
+      ksort($hours);
+      ksort($days);
+      jsonOutput(array(
+        'hours' => $hours,
+        'days' => $days
+      ));
+      break;
+    default:
+      jsonOutput(array('error' => 'unknown action `'.$_GET['action'].'`'));
+  }
+
+?>
---
+++ admin/index.php
@@ -0,0 +1,150 @@
+<?php
+
+require_once "../config.php";
+
+
+
+?>
+
+<script src="../js/jquery.js"></script>
+<script type="text/javascript" src="js/highcharts.js"></script>
+<script>
+  var $charts = {};
+  $(function _on_window_loaded () {
+    make_default_chart_containers($('body'), ['browsers', 'OS', 'days', 'hours']);
+
+    update_category_pie_chart ('browsers', 'Browser Ratio', $charts.browsers[0]);
+    update_category_pie_chart ('OS', 'OS Ratio', $charts.OS[0], 0.001);
+
+    $.get('ajax.php', {action: 'days-and-hours'}, function (response) {
+      category_line_chart('Daily activity', $charts.days[0], response.days);
+      category_line_chart('Hourly activity', $charts.hours[0], response.hours);
+    });
+
+  });
+
+  function make_default_chart_containers ($container, names) {
+    var i;
+    for (i=0; i<names.length; ++i) {
+      $charts[names[i]] = jq_element('div');
+      $charts[names[i]].attr({
+        'class': 'chart-container',
+        'chart-name': names[i]
+      });
+      $container.append($charts[names[i]])
+    }
+  }
+
+  function update_category_pie_chart (action, title, target, min_percent) {
+    // anything lower than 0.5 % will be added to "Other"
+    min_percent = min_percent === undefined ? 0.005 : min_percent;
+
+    return $.get('ajax.php', {
+      action: action
+    }, function (response) {
+      var tmp = {};
+      var data = [];
+      var i;
+      var value;
+      var total = 0;
+      var name;
+      var other = 0;
+      for (i=0; i<response.length; ++i) {
+        value = Number(response[i].value);
+        total += value;
+        if (response[i].category) {
+          tmp[response[i].category] = value;
+        } else {
+          other += value;
+        }
+      }
+      for (name in tmp) {
+        if (tmp[name] < min_percent * total) {
+          other += tmp[name];
+        } else {
+          data.push([name, tmp[name]]);
+        }
+      }
+      data.push(['Other', (tmp.Other || 0) + other]);
+      category_pie_chart(title + ' (total: '+total+')', target, data);
+    });
+  }
+
+  function category_line_chart (title, target, data) {
+    return new Highcharts.Chart({
+      chart: {
+        renderTo: target,
+        plotShadow: true
+      },
+      title: {
+        text: title
+      },
+      xAxis: {
+        categories: Object.keys(data)
+      },
+      tooltip: {
+        formatter: function() {
+          return '<b>'+this.y+'</b><br />('+this.x+')'
+        }
+      },
+      series: [{
+        name: 'value',
+        data: object_get_values(data)
+      }]
+    });
+  }
+
+  function category_pie_chart (title, target, data) {
+    return new Highcharts.Chart({
+      chart: {
+        renderTo: target,
+        plotShadow: true
+      },
+      title: {
+        text: title
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+        percentageDecimals: 2
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          showInLegend: true,
+          dataLabels: {
+            enabled: true,
+            color: '#000000',
+            connectorColor: '#000000',
+            formatter: function() {
+              return '<b>'+ this.point.name +'</b>: '+
+                      this.percentage.toFixed(2) +' % ('+this.y+')';
+            }
+          }
+        }
+      },
+      series: [{
+        type: 'pie',
+        name: 'share',
+        data: data
+      }]
+    });
+  }
+
+  function object_get_values (obj) {
+    var key;
+    var arr = [];
+    for (key in obj) {
+      arr.push(obj[key]);
+    }
+    return arr;
+  }
+
+  function jq_element (type) {
+    return $(document.createElement(type));
+  }
+</script>
+
+<body>
+
+</body>
